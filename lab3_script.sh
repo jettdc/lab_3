b1da53933dcc97367c832af6cba16d92f13d9f00
Jett <jecr4062@colorado.edu>
2020-02-12 10:55:27 -0700

    script

--- lab3_script.sh
+++ lab3_script.sh
@@ -13,8 +13,16 @@ grep $reg $filename
 echo "Problem 2:"
 echo "Number of phone numbers:"
 #3 numbers grouped together, a dash, another 3 numbers grouped together, a dash, 4 numbers grouped together.
-egrep -c '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}' $filename
+grep -c '[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}' $filename
 echo "Problem 3"
+#selects all letters before the @, then after until the .
 echo "Number of emails: "
 egrep -c '.+\@.+\..+' $filename
+echo "Phone numbers with 303"
+grep -o '303\-[0-9]\{3\}\-[0-9]\{4\}' $filename
+echo "Sending geocities emails to a file"
+egrep '.+\@geocities\.com' $filename >> email_results.txt
+git add email_results.txt
+git commit -m "emails"
+
 
